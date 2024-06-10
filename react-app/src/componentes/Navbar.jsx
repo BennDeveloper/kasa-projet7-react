87a6dcac88e7b2de56c1b7beb3a488bd736a8e70
bennabbou <bennabbou.a@gmail.com>
2024-06-10 12:10:35 +0200

    inisialisation

---
+++ react-app/src/componentes/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Navbar.css"
+
+function Navbar() {
+  return <nav className='navbar'>
+    <div className='navbar__logo'>
+        <img src="../LOGO.png" alt="logo" />
+    </div>
+  </nav>
+}
+
+export default Navbar
